4fca2cf74faabb40d6ff9aaa0eade4317351e9a8
vincpinas <ikbengerrit92@gmail.com>
2023-02-25 21:47:14 +0100

    :blue_heart: lottie animations added

--- src/Components/RobinIntro/RobinIntro.tsx
+++ src/Components/RobinIntro/RobinIntro.tsx
@@ -1,18 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import gsap from "gsap";
+import Lottie from "lottie-web";
 import "./RobinIntro.scss";
 import wave from "../../Assets/waves/wave.svg";
 import wave2 from "../../Assets/waves/wave2.svg";
 import wave3 from "../../Assets/waves/wave3.svg";
 import wave4 from "../../Assets/waves/wave4.svg";
-import gsap from "gsap";
 
 function RobinIntro() {
-  let LogoTL: GSAPTimeline;
+  let NavTL: GSAPTimeline;
+  const sunRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-    LogoTL = gsap.timeline({
+    NavTL = gsap.timeline({
       scrollTrigger: {
         trigger: ".c-robinintro",
         start: "center top",
@@ -24,20 +26,29 @@ function RobinIntro() {
         delay: 0,
       }, 1)
 
+    if (sunRef.current) {
+      Lottie.loadAnimation({
+        container: sunRef.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        path: "./Assets/lottie/sun-icon.json"
+      })
+    }
 
     return () => {
-      LogoTL.clear();
-      LogoTL.kill();
+      NavTL.kill();
     }
   }, [])
 
   return (
     <div className="c-robinintro -page">
+      <div ref={sunRef} className="c-robinintro__sun"></div>
       <div className="c-robinintro__ocean">
-        <span className="c-robinintro__wave -fourth" style={{background: `url(${wave4}) repeat-x`}} />
-        <span className="c-robinintro__wave -third" style={{background: `url(${wave3}) repeat-x`}} />
-        <span className="c-robinintro__wave -second" style={{background: `url(${wave2}) repeat-x`}} />
-        <span className="c-robinintro__wave -first" style={{background: `url(${wave}) repeat-x`}} />
+        <span className="c-robinintro__wave -fourth" style={{ background: `url(${wave4}) repeat-x` }} />
+        <span className="c-robinintro__wave -third" style={{ background: `url(${wave3}) repeat-x` }} />
+        <span className="c-robinintro__wave -second" style={{ background: `url(${wave2}) repeat-x` }} />
+        <span className="c-robinintro__wave -first" style={{ background: `url(${wave}) repeat-x` }} />
       </div>
     </div>
   )
